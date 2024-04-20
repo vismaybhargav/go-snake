b320e143d7c3211e200fa42481dd00f8993f4b72
vacharbhargav008 <vacharbhargav008@student.fuhsd.org>
2024-04-20 03:44:45 -0700

    Implemented rendering the Game

--- snake.go
+++ snake.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type Game struct {
 	width, height int
@@ -9,36 +12,57 @@ type Game struct {
 	p             *Player
 }
 
-func (g Game) Create(width int, height int) {
+// Initializes the game given a width and height
+func (g *Game) Create(width int, height int) {
 	g.width = width
 	g.height = height
 
-	p := &Player{
-		x:      make([]int, width*height),
-		y:      make([]int, width*height),
-		pieces: 1,
-	}
-	g.p = p
-
-	a := &Apple{
-		x: 1,
-		y: 1,
-	}
-	g.a = a
-
-	var grid [][]int = make([][]int, width*height)
+	// Create the grid for the game
+	var grid [][]int = make([][]int, width)
 	for i := 0; i < len(grid); i++ {
 		grid[i] = make([]int, height)
 	}
 	g.grid = grid
+
+	// Create the player
+	g.p = &Player{
+		x:      make([]int, width*height),
+		y:      make([]int, width*height),
+		pieces: 1,
+	}
+	g.p.x[0] = rand.Intn(width)
+	g.p.y[0] = rand.Intn(height)
+
+	// Create the Apple
+	g.a = &Apple{
+		x: rand.Intn(width),
+		y: rand.Intn(height),
+	}
+
+	for g.a.x == g.p.x[0] && g.a.y == g.p.y[0] {
+		g.a.x = rand.Intn(width)
+		g.a.y = rand.Intn(height)
+	}
 }
 
-func (g Game) Render() {
+func (g *Game) Render() {
 	for i := 0; i < g.width+2; i++ {
-		if i == 0 {
-			fmt.Print("*")
-		}
+		fmt.Print("#")
 	}
+	fmt.Println("")
+
+	for i := 0; i < g.height; i++ {
+		fmt.Print("#")
+		for j := 0; j < g.width; j++ {
+			fmt.Print(" ")
+		}
+		fmt.Println("#")
+	}
+
+	for i := 0; i < g.width+2; i++ {
+		fmt.Print("#")
+	}
+	fmt.Println("")
 }
 
 type Player struct {
@@ -51,6 +75,8 @@ type Apple struct {
 }
 
 func main() {
+	// init the game
 	var g Game
-	g.Create(20, 20)
+	g.Create(40, 20)
+	g.Render()
 }
